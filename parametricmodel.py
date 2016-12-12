dd0a42d16e225721eafbba09e11ba836f22c053d
Rich Teague <teague@mpia.de>
2016-12-12 16:45:51 +0100

    Changed to gridded models.

--- parametricmodel.py
+++ parametricmodel.py
@@ -1,100 +1,211 @@
-# Functions to create a parametric protoplanetary disk model.
-# Based mainly on Rosenfeld et al. (2013) and Flaherty et al. (2015).
-# Focusses on the gas. Dust can be scaled appropriately.
-
 import numpy as np
 import scipy.constants as sc
+from scipy.ndimage.interpolation import map_coordinates
 
-
-# Pressure scale height based on midplane temperature.
-def getScaleHeight(r, q_mid, T_mid, mu=2.37, Mstar=0.6):
-    Tm = T_mid * np.power(r / 10., -q_mid)
-    Hp = sc.k * Tm * r**3 * sc.au / mu / sc.m_p / sc.G / Mstar / 1.99e30
-    return np.sqrt(Hp)
-
-
-# Gas temperature from two power-laws.
-def getTemperature(r, z, q_mid, q_atm, T_mid, T_atm, mu=2.37, delta=1.):
-    #
-    # Calculate the individual power law profiles.
-    #
-    Ta = getTemperaturePL(r, q_atm, T_atm)[None, :] * np.ones(z.size)[:, None]
-    Tm = getTemperaturePL(r, q_mid, T_mid)[None, :] * np.ones(z.size)[:, None]
-    #
-    # Find the pressure scale height for the turnover.
-    #
-    zq = 4. * getScaleHeight(r, q_mid, T_mid, mu=mu)
-    zq = zq[None, :] * np.ones(Ta.shape)
-    #
-    # Calculate the intermediate temperatures.
-    #
-    zvals = z[:, None] * np.ones(zq.shape)
-    Ti = Ta + (Tm - Ta) * np.cos(np.pi * zvals / 2. / zq)**(2.*delta)
-    return np.where(zvals > zq, Ta, Ti)
-
-
-# Get the temperature power law.
-def getTemperaturePL(r, q, T):
-    return T * np.power(r / 10., -q)
-
-
-# Surface density profile.
-def getSurfaceDensity(r, sigma_c, r_c, gamma):
-    sigma = sigma_c * np.power(r/r_c, -gamma)
-    return sigma * np.exp(-1. * np.power(r/r_c, 2-gamma))
-
-
-# The sound speed of the gas.
-def getSoundSpeed(temp, mu=2.37):
-    return np.sqrt(sc.k * temp / mu / sc.m_p)
-
-
-# Density structure assuming hydrostatic equilibrium.
-def getNonIsothermalDensity(rvals, zvals, temp, sigma, Mstar=0.6):
-    #
-    # Check that the temperature array is the correct shape and size.
-    #
-    if temp.ndim != 2:
-        raise ValueError("temp must be 2D.")
-    elif temp.shape != (zvals.size, rvals.size):
-        raise ValueError("temp must be on (rvals, zvals) grid.")
-    #
-    # Iteratively solve the eequation of hydrostatic equilibrium.
-    #
-    dens = np.ones(temp.shape)
-    for i in range(1, int(zvals.size)):
-        dlnT = np.log(temp[i-1] / temp[i])
-        dlnT = np.where(np.isfinite(dlnT), dlnT, 0.0)
-        dzdc = (zvals[i-1] - zvals[i]) * sc.au
-        dzdc /= getSoundSpeed(temp[i])**2.
-        dzdc = np.where(np.isfinite(dzdc), dzdc, 0.0)
-        grav = sc.G * Mstar * 1e30 * zvals[i] * sc.au
-        grav /= np.hypot(rvals * sc.au, zvals[i] * sc.au)**3.
+class ppd:
+    
+    def __init__(self, r_c, T_mid, T_atm, q_mid, q_atm, gamma, **kwargs):
+        """A parametric disk model as used by Williams and Best (2014)."""
+        
+        # User set values.
+        self.r_c = r_c
+        self.T_mid = T_mid
+        self.T_atm = T_atm
+        self.q_mid = q_mid
+        self.q_atm = q_atm
+        self.gamma = gamma
+        
+        # System related defaults.
+        self.msun = 1.989e30
+        self.mdisk = kwargs.get('mdisk', 0.01) * self.msun
+        self.mstar = kwargs.get('mstar', 1.) * self.msun
+        
+        # Grid defaults.
+        self.dgrid = kwargs.get('dgrid', 1.0)
+        self.r_min = kwargs.get('r_min', self.dgrid)
+        self.r_max = kwargs.get('r_max', 15.)
+        self.z_max = kwargs.get('z_max', 10.)
+        self.frame = kwargs.get('frame', 10)
+        self.shape = kwargs.get('shape', 'full')
+        
+        # Tempearture and density related defaults.
+        self.nzq = kwargs.get('nzq', 4.0)
+        self.mu = kwargs.get('mu', 2.34)
+        self.delta = kwargs.get('delta', 1.0)
+        self.gamma = kwargs.get('gamma', 1.0)
+        self.mindens = kwargs.get('mindens', 1e4)    
+        self.filldens = kwargs.get('filldens', 0.0)
+        self.filltemp = kwargs.get('filltemp', 0.0)
+        
+        # Abundance related defaults.
+        self.freezeout = kwargs.get('freezeout', 20.)
+        self.ndiss = kwargs.get('ndiss', 1.3e21)
+        self.xgas = kwargs.get('xgas', 1e-4)
+        self.xice = kwargs.get('xice', 1e-9)
+        
+        # Calculate the model.
+        self.rgrid, self.zgrid = self.get_grids()
+        self.temperature = self.get_temperature(self.rgrid, self.zgrid)
+        self.density = self.get_density(self.rgrid, self.zgrid)
+        self.trim_grid()
+        self.temperature = np.where(self.density != self.filldens, 
+                                    self.temperature,
+                                    self.filltemp)
+        self.frozen = self.get_frozen(self.freezeout)
+        self.dissociated = self.get_dissociated(self.ndiss)
+        self.abundance = self.get_abundance(self.xgas, self.xice)
+        self.molecular_density = self.abundance * self.density
+        
+        # If appropriate, mirror the model.
+        if self.shape == 'full':
+            self.mirror_model()
+        return
+    
+    # Functions.
+    
+    def mirror_array(self, array):
+        """Mirror a single array. Include r=0 column and do 
+        not duplicate z=0 column."""    
+        array = np.vstack([np.flipud(array), array[1:]])
+        array = np.hstack([np.zeros(array.shape[0])[:, None], array])
+        return np.hstack([np.fliplr(array), array[:,1:]])
+    
+    def mirror_model(self):
+        """Mirror the model in r = 0 and z = 0 planes."""
+        self.temperature = self.mirror_array(self.temperature)
+        self.density = self.mirror_array(self.density)
+        self.abundance = self.mirror_array(self.abundance)
+        self.frozen = self.mirror_array(self.frozen)
+        self.dissociated = self.mirror_array(self.dissociated)
+        self.molecular_density = self.mirror_array(self.molecular_density)
+        self.rgrid = np.insert(self.rgrid, 0, 0)
+        self.rgrid = np.insert(self.rgrid[1:], 0, -1*self.rgrid[::-1])
+        self.zgrid = np.insert(self.zgrid[1:], 0, -1*self.zgrid[::-1])
+        return
+    
+    def trim_grid(self):
+        """Trim the grids to get rid of excess edges."""
+        disk = np.where(self.density != self.filldens, 1, 0)
+        trim_z = np.sum(disk, 1)
+        trim_r = np.sum(disk, 0)
+        flag = 0 
+        if trim_r[-1] != 0:
+            print 'Radial grid not large enough. Increase r_max.'
+            flag +=1
+        if trim_z[-1] != 0:
+            print 'Vertical grid not large enough. Increase z_max.'
+            flag += 1
+        if flag:
+            return
+        for z in range(1,trim_z.size+1):
+            if trim_z[-z] > 0:
+                break
+        for r in range(1,trim_r.size+1):
+            if trim_r[-r] > 0:
+                break
+        if (z < self.frame or r < self.frame):
+            return
+        z = trim_z.size - z + self.frame
+        r = trim_r.size - r + self.frame
+        self.zgrid = self.zgrid[:z]
+        self.rgrid = self.rgrid[:r]
+        self.temperature = self.temperature[:z,:r]
+        self.density = self.density[:z,:r]
+        return
+    
+    def get_grids(self):
+        """Returns the r and z grids which things are calculated."""
+        rgrid = np.arange(self.dgrid, self.r_c * self.r_max + self.dgrid, self.dgrid)
+        zmax = self.scaleheight(rgrid[1:]).max()
+        zgrid = np.arange(0, zmax * self.z_max + self.dgrid, self.dgrid)
+        return rgrid, zgrid
+    
+    def soundspeed(self, rgrid, zgrid):
+        """Soundspeed of the gas in [m/s]."""
+        return np.sqrt(sc.k * self.get_temperature(rgrid, zgrid) / self.mu / sc.m_p)
+    
+    def scaleheight(self, rgrid):
+        """Pressure scale height of gas in [au]."""
+        Tmid = self.temp_midplane(rgrid)
+        Hp = sc.k * Tmid * rgrid**3 * sc.au / self.mu / sc.m_p / sc.G / self.mstar
+        return np.sqrt(Hp)
+    
+    def surfacedensity(self, r):
+        """Surface density of gas [g / sqcm]."""       
+        sigma = np.exp(self.powerlaw(r, self.r_c, -1., 2.-self.gamma))
+        sigma *= self.powerlaw(r, self.r_c, 1., -self.gamma)
+        sigma *= self.mdisk * (2. - self.gamma)
+        sigma /= 2. * np.pi * np.power(self.r_c * sc.au * 100, 2.) / 1e3
+        return np.where(r >= self.r_min, sigma, 0.0)
+    
+    def temp_midplane(self, r):
+        """Returns the midplane temperature [K] at r [au]."""
+        temp = self.powerlaw(r, self.r_c, self.T_mid, -self.q_mid)
+        return np.where(r >= self.r_min, temp, 0.0)
+    
+    def temp_atmosphere(self, r):
+        """Returns the atmospheric temperature [K] at r [au]."""
+        temp =  self.powerlaw(r, self.r_c, self.T_atm, -self.q_atm)
+        return np.where(r >= self.r_min, temp, 0.0)
+    
+    def get_temperature(self, rgrid, zgrid):
+        """Calculate the temperature on the provided grid axes."""
+        temp_mid = self.temp_midplane(rgrid)
+        temp_atm = self.temp_atmosphere(rgrid)
+        zq = self.nzq * self.scaleheight(rgrid)
+        T = np.sin(np.pi * zgrid[:, None] / 2. / zq[None, :])
+        T = np.power(T, 2. * self.delta)
+        T *= temp_atm[None, :] - temp_mid[None, :]
+        T += temp_mid[None, :]
+        return np.where(zgrid[:, None] > zq[None, :], temp_atm[None, :], T)    
+    
+    def get_density(self, rgrid, zgrid):
+        """Calculate the density [n(H2)] structure on the provided grid axes."""
+        T = np.log(self.get_temperature(rgrid, zgrid + self.dgrid))
+        T -= np.log(self.get_temperature(rgrid, zgrid - self.dgrid))
+        T /= 2. * self.dgrid * sc.au
+        T = np.where(np.isfinite(T), T, 0.0)
+        cs = np.power(self.soundspeed(rgrid, zgrid), -2.)
+        cs = np.where(np.isfinite(cs), cs, 0.0)
+        grav = sc.G * self.mstar * zgrid * sc.au
+        grav = grav[:, None] / np.hypot(rgrid[None, :] * sc.au, zgrid[:, None] * sc.au)**3.
         grav = np.where(np.isfinite(grav), grav, 0.0)
-        dens[i] = dens[i-1] * np.exp(dlnT + dzdc*grav)
-    #
-    # Normalise each radial point to the surface density.
-    #
-    for i in range(rvals.size):
-        dens[:, i] *= sigma[i] / np.trapz(dens[:, i], x=zvals*sc.au*100.,
-                                          axis=0)
-    return dens
+        # TODO: Is there a way to speed this bit up?
+        rho = np.ones(T.shape)
+        for i in range(0, rgrid.size):
+            for j in range(1, zgrid.size):
+                rho[j,i] = np.log(rho[j-1,i]) 
+                rho[j,i] -= self.dgrid * sc.au * (grav[j,i] * cs[j,i] + T[j,i]) 
+                rho[j,i] = np.exp(rho[j,i])
+        rho -= np.nanmin(rho)
+        rho /= np.trapz(rho, zgrid * sc.au * 100., axis=0)
+        rho *= self.surfacedensity(rgrid)[None, :] 
+        rho /= self.mu * sc.m_p * 1e3
+        return np.where(rho >= self.mindens, rho, self.filldens)
 
-
-# Density structure assuming vertically isothermal disks.
-def getIsothermalDensity(zvals, sigma, scaleheight):
-    dens = np.power(zvals[:, None], 2) / 2. / np.power(scaleheight[None, :], 2)
-    dens = np.exp(-dens)
-    dens *= sigma / scaleheight / sc.au / 100. / np.sqrt(2. * np.pi)
-    return dens
-
-
-# Get the CO parameterised CO abundance.
-def getCOAbundance(ygrid, dens, temp, x_gas=1e-4, x_ice=0.0,
-                   T_freeze=20., N_diss=1.3e21):
-    dens = np.where(np.isfinite(dens), dens, 0.0)
-    temp = np.where(np.isfinite(temp), temp, 0.0)
-    N_H2 = np.array([np.trapz(dens[i:], x=ygrid[i:]*sc.au*100., axis=0)
-                     for i in range(ygrid.size)])
-    return np.where(np.logical_and(temp > T_freeze, N_H2 > N_diss),
-                    x_gas*dens, x_ice*dens)
+    def get_abundance(self, gas_abundance, ice_abundance):
+        """Return the molecular abundance."""
+        return np.where(self.frozen + self.dissociated == 0, gas_abundance, ice_abundance)
+    
+    def get_frozen(self, freezeout_temp):
+        """Return boolean of if the molecule is frozen."""
+        return np.where(self.temperature <= freezeout_temp, 1, 0)
+    
+    def get_dissociated(self, ndiss):
+        """Return a boolean of if the molecule is shielded."""
+        shield = [[np.trapz(self.density[j:,i], self.zgrid[j:]*sc.au*100.)
+                       for i in range(self.rgrid.size)]
+                      for j in range(self.zgrid.size)]
+        shield = np.array(shield)
+        return np.where(shield <= ndiss, 1, 0)
+    
+    def interp(self, rpnts, zpnts, parameter, **kwargs):
+        """Interpolates the 2D array: parameter."""
+        ridx = np.interp(rpnts, self.rgrid, np.arange(self.rgrid.size))
+        zidx = np.interp(zpnts, self.zgrid, np.arange(self.zgrid.size))
+        return map_coordinates(parameter, [zpnts, rpnts], order=1, **kwargs)
+    
+    
+    @staticmethod
+    def powerlaw(x, x0, a, b):
+        return a * np.power(x / x0, b)
---
+++ rays.py
@@ -0,0 +1,48 @@
+import numpy as np
+import scipy.constants as sc
+from parametricmodel import ppd
+from scipy.ndimage.interpolation import map_coordinates
+
+class rays:
+    """Trace rays through a disk model and return physical 
+    properties along those rays."""
+    
+    def __init__(self, ppd_instance):
+        self.model = ppd_instance
+        self._rays = {}
+        return 
+
+    def get_ray(self, intercept, angle):
+        """Returns a ray instance."""
+        try:
+            return self._rays[intercept, angle]
+        except:
+            pass
+        coords = self.ray_coords(intercept, angle)            
+        self._rays[intercept, angle] = self.ray_trace(coords)
+        return self._rays[intercept, angle]
+    
+    def ray_coords(self, intercept, angle):
+        """Returns the (r,z) coordinates of the ray."""
+        z0 = np.nanmin(self.model.zgrid)
+        z1 = np.nanmax(self.model.zgrid)
+        dr = np.tan(np.radians(angle)) * z1
+        r0 = intercept - dr
+        r1 = intercept + dr
+        nsteps = np.ceil(np.hypot((r1 - r0), (z1 - z0)) / self.model.dgrid)
+        rpnts = np.linspace(r0, r1, nsteps)
+        zpnts = np.linspace(z0, z1, nsteps)
+        in_grid = np.logical_and(rpnts >= self.model.rgrid.min(), rpnts <= self.model.rgrid.max())
+        rpnts = rpnts[in_grid]
+        zpnts = zpnts[in_grid]
+        return rpnts, zpnts
+    
+    def ray_trace(self, coords):
+        """Returns a dictionary along the path."""
+        ray = {}
+        ray['rpnts'] = coords[0]
+        ray['zpnts'] = coords[1]
+        ray['density'] = self.model.interp(coords[0], coords[1], self.model.density)
+        ray['temperature'] = self.model.interp(coords[0], coords[1], self.model.temperature)
+        ray['abundance'] = self.model.interp(coords[0], coords[1], self.model.abundance)
+        return ray
